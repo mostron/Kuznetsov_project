f8fed25b1a47155857e1355b2a573db6aa92c383
Кузнецов Дмитрий <vyatskiy.d@mail.ru>
2026-03-10 17:45:51 +0300

    Выполнено Task_1

---
+++ task_1.py
@@ -0,0 +1,21 @@
+student_name = "Дмитрий Кузнецов"
+group_number = 502
+project_name = "ЖК Распутин"  
+floors = 24
+height = 72
+is_residential = True
+construction_year = 2022
+
+total_height = floors * height  
+
+print(f"== ПАСПОРТ СТРОИТЕЛЬНОГО ОБЪЕКТА ==\n"
+      f"Составитель: {student_name}\n"
+      f"Группа: {group_number}\n"
+      f"Объект: {project_name}\n"
+      f"Этажность: {floors} этажей\n"
+      f"Высота: {total_height} м\n"
+      f"Тип: {'Жилой' if is_residential else 'Нежилой'}\n"
+      f"Год постройки: {construction_year}")
+
+# 13-я линия Васильевского острова, 50, Санкт-Петербург
+# Проходил мимо, понравился
